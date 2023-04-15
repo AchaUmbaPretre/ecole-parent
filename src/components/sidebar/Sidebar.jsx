75bb315a81ac8eafcfe6b26d5dbee2043d3488cd
achaumba <achandambi@gmail.com>
2023-04-15 05:58:14 -1100

    commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 import './sidebar.css'
-import { LineStyle,ListAltOutlined, PublishedWithChangesTwoTone } from '@mui/icons-material'
+import { LineStyle,ListAltOutlined, Person, Person3, Person3Outlined, Person4TwoTone, PublishedWithChangesTwoTone } from '@mui/icons-material'
 
 const Sidebar = () => {
   return (
@@ -12,7 +12,10 @@ const Sidebar = () => {
                 <h3 className="sidebarH3">Dashboard</h3>
                 <ul className="sidebarUl">
                   <Link to={'/'}><li className="sidebarLi active"><LineStyle className="sidebarIcon"/>Accueil</li></Link>
-                  <Link to={'/users'}><li className="sidebarLi"><PublishedWithChangesTwoTone className="sidebarIcon"/>Publications</li></Link>
+                  <Link to={'/eleves'}><li className="sidebarLi"><Person3Outlined className="sidebarIcon"/>Eleves</li></Link>
+                  <Link to={'/inscription'}><li className="sidebarLi"><PublishedWithChangesTwoTone className="sidebarIcon"/>Inscription</li></Link>
+                  <Link to={'/professeur'}><li className="sidebarLi"><Person4TwoTone className="sidebarIcon"/>Professeurs</li></Link>
+                  <Link to={'/publications'}><li className="sidebarLi"><PublishedWithChangesTwoTone className="sidebarIcon"/>Publications</li></Link>
                   <Link to={'/presence'} ><li className="sidebarLi"><ListAltOutlined  className="sidebarIcon"/>Présences</li></Link>
                 </ul>
               </div>

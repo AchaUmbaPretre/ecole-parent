dc8f1df2bf4a9966aaf91ee348330759cfdf457f
achaumba <achandambi@gmail.com>
2023-04-18 12:10:02 -1100

    commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,29 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './login.css'
 
 const Login = () => {
+
+  const [login, setLogin] = useState({
+    nom : '',
+    postnom: ''
+  })
+const handChange = (e) =>{
+  setLogin((prev) =>({...prev, [e.target.name]:e.target.value}))
+}
+
   return (
     <>
         <div className="login">
           <div className="login-container">
             <h1 className="login-h1">Sign In</h1>
             <div className="form-div">
-              <input type="text" className="form-input" placeholder='' />
+              <input type="text" className="form-input" placeholder='' name='nom' onChange={handChange} />
               <label htmlFor="" className="form-label">Nom</label>
             </div>
 
             <div className="form-div">
-              <input type="text" className="form-input" placeholder='' />
+              <input type="text" className="form-input" placeholder='' name='password' onChange={handChange} />
               <label htmlFor="" className="form-label">Password</label>
             </div>
             <button className="login-btn">envoyer</button>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,22 +1,34 @@
+import { useState } from 'react'
 import './register.css'
 
 const Register = () => {
+  
+  const [register, setRegister] = useState({
+      nom:'',
+      email: '',
+      password: ''
+  });
+
+  const handChange =(e)=>{
+    setRegister((prev)=>({...prev, [e.target.name]:e.target.value}));
+  }
+  console.log(register)
   return (
     <>
         <div className="login">
           <div className="login-container">
             <h1 className="login-h1">Registrer</h1>
             <div className="form-div">
-              <input type="text" className="form-input" placeholder='' />
+              <input type="text" className="form-input" placeholder='' name="nom" onChange={handChange} />
               <label htmlFor="" className="form-label">Nom</label>
             </div>
 
             <div className="form-div">
-              <input type="text" className="form-input" placeholder='' />
+              <input type="text" className="form-input" placeholder='' name="email" onChange={handChange} />
               <label htmlFor="" className="form-label">Email</label>
             </div>
             <div className="form-div">
-              <input type="text" className="form-input" placeholder='' />
+              <input type="text" className="form-input" placeholder='' name="password" onChange={handChange} />
               <label htmlFor="" className="form-label">Password</label>
             </div>
             <button className="login-btn">register</button>
